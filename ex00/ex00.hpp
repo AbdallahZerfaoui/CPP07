09cd33006ec568604796549881f793851a94a97a
AbdallahZerfaoui <zerfaouiabdallah@gmail.com>
2025-10-19 17:12:25 +0200

    first commit

---
+++ ex00/ex00.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void swap(T& x, T& y);
+
+template <typename T>
+T min(T x, T y);
+
+template <typename T>
+T max(T x, T y);
+
+#include "ex00.tpp"
